c29cc8704a912ebb556028cc6b59c899c428ae62
SaintJo <panchenkovanya@gmail.com>
2023-04-18 12:27:00 +0300

    clear out the input field

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let myLeads = ["www.awesomelead.com ", "www.epiclead.com ", "www.greatlead.com "];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
@@ -7,19 +7,23 @@ const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function() {
 	myLeads.push(inputEl.value);
-	console.log(myLeads);
+	// Clear out the input field
+	inputEl.value = "";
+	renderLeads();
 });
 
-let listItems = "";
 
-for (let i = 0; i < myLeads.length; i++) {
-	listItems+= "<li>" + myLeads[i] + "</li>";
+function renderLeads () {
+	let listItems = "";
+	for (let i = 0; i < myLeads.length; i++) {
+		listItems+= "<li>" + myLeads[i] + "</li>";
 	
-	// Create element li
-	// const li = document.createElement("li");
-	// li.textContent = myLeads[i];
-	// ulEl.append(li);
+		// Create element li
+		// const li = document.createElement("li");
+		// li.textContent = myLeads[i];
+		// ulEl.append(li);
 
+	}
+
+	ulEl.innerHTML = listItems;
 }
-
-ulEl.innerHTML = listItems;
