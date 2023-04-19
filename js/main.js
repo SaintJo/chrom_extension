1cac0c5873334f23b10f1c812f8ce02cf8ebc84a
SaintJo <panchenkovanya@gmail.com>
2023-04-19 21:07:21 +0300

    add a function on delete button

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
@@ -9,6 +10,12 @@ if (leadsFromLocalStorage) {
 	renderLeads();
 }
 
+
+deleteBtn.addEventListener("dblclick", function(){
+	localStorage.clear();
+	myLeads = [];
+	renderLeads();
+})
 
 inputBtn.addEventListener("click", function() {
 	myLeads.push(inputEl.value);
