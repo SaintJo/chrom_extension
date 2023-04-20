4d20a05784a7188918f571afcab8aca6ec30cc10
SaintJo <panchenkovanya@gmail.com>
2023-04-20 08:18:32 +0300

    add a function to get the current tab URL

--- js/main.js
+++ js/main.js
@@ -46,7 +46,16 @@ inputBtn.addEventListener("click", function() {
 });
 
 tabBtn.addEventListener("click", function(){
-	console.log(tabs[0].url);
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+	
+	// since only one tab should be active and in the current window at once
+  	// the return variable should only have one entry
+  	let activeTab = tabs[0];
+  	let activeTabId = activeTab.id; // or do whatever you need
+});
+	myLeads.push(tabs[0].url);
+	localStorage.setItem("myLeads", JSON.stringify(myLeads));
+	render(myLeads);
 })
 
 
